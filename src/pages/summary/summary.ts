41ee36552269f87a28641669cfd0890d07183a4b
Pedro <pinfante@itssglobal.com>
2018-04-09 21:48:44 -0400

    Added income summary and labels

--- src/pages/summary/summary.ts
+++ src/pages/summary/summary.ts
@@ -20,6 +20,7 @@ export class SummaryPage {
 
   @ViewChild('doughnutCanvas') doughnutCanvas;
   doughnutChart: any;
+  doughnutIncome: any;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
@@ -28,7 +29,7 @@ export class SummaryPage {
  
       type: 'doughnut',
       data: {
-          labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+          labels: ["Restaurants", "Debts", "Bills", "Electricity", "Education", "Entertainment"],
           datasets: [{
               label: '# of Votes',
               data: [12, 19, 3, 5, 2, 3],
@@ -52,6 +53,35 @@ export class SummaryPage {
       }
 
   });
+
+  this.doughnutIncome = new Chart(this.doughnutCanvas.nativeElement, {
+ 
+    type: 'doughnut',
+    data: {
+        labels: ["Salary", "Loans", "Gifts", "Income(1)", "Income(2)", "Income(3)"],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            hoverBackgroundColor: [
+                "#FF6384",
+                "#36A2EB",
+                "#FFCE56",
+                "#FF6384",
+                "#36A2EB",
+                "#FFCE56"
+            ]
+        }]
+    }
+
+});
     console.log('ionViewDidLoad SummaryPage');
   }
 
